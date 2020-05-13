5270e833002256fef7f0b6c19931d85a4f73d61b
vicdubbeld <vicdubbeld@gmail.com>
2020-05-13 14:36:14 -0400

    tailwind css added

--- todo-list/resources/views/todos/create.blade.php
+++ todo-list/resources/views/todos/create.blade.php
@@ -1,1 +1,19 @@
-Create
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Todos</title>
+</head>
+<body>
+    <div>
+    <h1>Add a task here</h1>
+    <form action="post" action="todo/create">
+        <input type="text" name="title" />
+        <input type="submit" value="Create" />
+    </form>
+</div>
+</body>
+</html>

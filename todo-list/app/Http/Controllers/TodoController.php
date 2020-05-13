c752943fa3810c2bc570e570416981ba2cc26591
vicdubbeld <vicdubbeld@gmail.com>
2020-05-13 16:23:29 -0400

    Now able to add tasks

--- todo-list/app/Http/Controllers/TodoController.php
+++ todo-list/app/Http/Controllers/TodoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class TodoController extends Controller
 {
     public function index()
--- todo-list/resources/views/todos/create.blade.php
+++ todo-list/resources/views/todos/create.blade.php
@@ -10,7 +10,8 @@
 <body>
     <div class="text-center pt-10">
     <h1 class="text-2xl">Add a task here</h1>
-    <form action="post" action="todo/create" class="py-5">
+    <form method="post" action="/todos/create" class="py-5">
+        @csrf
         <input type="text" name="title" class="py-2 px-2 border rounded" />
         <input type="submit" value="Create" class="p-2 border rounded-lg" />
     </form>
